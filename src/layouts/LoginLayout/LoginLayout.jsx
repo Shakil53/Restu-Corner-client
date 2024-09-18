f740f1aa26d11a8cecbd47ac494627676752724c
Shakil53 <shakilahmed.dcc@gmail.com>
2024-09-18 23:58:17 +0600

    layout decratation

---
+++ src/layouts/LoginLayout/LoginLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../../pages/Shared/Header/Header';
+
+
+const LoginLayout = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+
+        </div>
+    );
+};
+
+export default LoginLayout;
